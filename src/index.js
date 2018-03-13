075b90789519901024656244f22d2a88f7253236
hannahhopp <hannah.hopp@gmail.com>
2018-03-13 13:01:16 -0700

    map added

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('I ran correctly!')
+const mapboxgl = require("mapbox-gl");
+
+mapboxgl.accessToken = "pk.eyJ1Ijoib25pZmF1eCIsImEiOiJjamVxMmxsbTIwMmp3MzNwZnk0Nm1taDFsIn0.yC6izMsKXtTQcUL3FBbEcA";
+
+const map = new mapboxgl.Map({
+  container: "map",
+  center: [-74.009, 40.705], // FullStack NY coordinates; alternatively, use [-87.6354, 41.8885] for Chicago
+  zoom: 12, // starting zoom
+  style: "mapbox://styles/mapbox/streets-v10" // mapbox has lots of different map styles available.
+});
